67f2bf73ee52e8ab0228088ba3c8b26028181970
morenoaljon-GOCEBU <morenoaljon@gmail.com>
2026-01-30 07:08:14 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-white text-gray-900 flex items-center justify-around py-[10vh]">
+    <div className="min-h-screen bg-white text-gray-900 flex items-middle justify-around py-[15vh]">
       
       {/* LUXURY EMBOSSED BOX */}
       <div className="w-[80%] bg-stone-50 rounded-3xl shadow-[0_20px_60px_rgba(0,0,0,0.12)] border border-stone-200">
