97a1baa47c052f3463c0edcd79528504574ee63c
morenoaljon-GOCEBU <morenoaljon@gmail.com>
2026-01-30 05:48:29 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './index.css'     // 👈 MUST be here
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
